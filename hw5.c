933198c745b5bd51aebd98963522ec34b76f9aff
junyong <21300649@handong.edu>
2018-05-31 09:45:54 +0900

    console form added

--- hw5.c
+++ hw5.c
@@ -4,7 +4,6 @@
 #include <stddef.h>
 #include <time.h>
 
-#define numOfVertex 10
 
 typedef struct Edge{
 	int vertex[2];
@@ -12,10 +11,13 @@ typedef struct Edge{
 	int visited;
 }Edge;
 
-int d[10];
-int parent[10];
-int visited[10];
+int d[30];
+int parent[30];
+int visited[30];
+int vNum=0;
 Edge edge[200];
+char order[30][100]; //index for vertex name since we'll consider all vertex as integer in the program.
+
 
 void relax(u, v, weight){
 	if(d[v] > d[u]+weight){
@@ -24,7 +26,7 @@ void relax(u, v, weight){
 	}
 }	
 
-void dijkstra(int matrix[][11], int source){
+void dijkstra(int matrix[][30], int source){
 	
 	int i;
 	int j;
@@ -32,21 +34,21 @@ void dijkstra(int matrix[][11], int source){
 	int u;
 	int sum = 0;	
 
-	for(i=0;i<10;i++){
+	for(i=0;i<vNum;i++){
 		d[i] = 10000;
 	}
 	d[s] = 0;
 	
-	for(i=0;i<10;i++)
+	for(i=0;i<vNum;i++)
 		visited[i] = 0;
 
 	int idx;
-	while(sum != 10){
+	while(sum != vNum){
 	
-		for(i=0;i<10;i++){
+		for(i=0;i<vNum;i++){
 			if(visited[i]==0){
 				int min = d[i];
-				for(j=0;j<10;j++){
+				for(j=0;j<vNum;j++){
 					if(visited[j]==0){
 						if(min >= d[j]){
 							min = d[j];
@@ -62,7 +64,7 @@ void dijkstra(int matrix[][11], int source){
 		
 //		printf("%d \n",u);
 		
-		for(i=0;i<10;i++){
+		for(i=0;i<vNum;i++){
 			if(matrix[u+1][i+1]>0 && matrix[u+1][i+1]<10000)
 				relax(u, i, matrix[u+1][i+1]);
 		}
@@ -78,16 +80,16 @@ int Bellman_Ford(int source, int size){
 	int s = source;
 	int sum = 0;	
 	
-	for(i=0;i<10;i++){
+	for(i=0;i<vNum;i++){
 		d[i] = 10000;
 	}
 	d[s] = 0;
 
-	for(i=0;i<50;i++){
+	for(i=0;i<200;i++){
 		edge[i].visited = 0;
 	}
 	
-	for(i=0;i<9;i++){
+	for(i=0;i<vNum-1;i++){
 		for(j=0;j<size;j++){
 			relax(edge[j].vertex[0],edge[j].vertex[1],edge[j].weight);
 		}	
@@ -109,21 +111,21 @@ int Bellman_Ford(int source, int size){
 	return 1;
 }
 
-void Floyd(int matrix[][11]){
+void Floyd(int matrix[][30]){
 
 	int i,j,k;
-	int D[10][10];
+	int D[30][30];
 
-	for(i=0;i<10;i++){
-		for(j=0;j<10;j++){
+	for(i=0;i<vNum;i++){
+		for(j=0;j<vNum;j++){
 			D[i][j] = matrix[i+1][j+1];
 		}
 	}
 
 
-	for(i=0;i<10;i++){
-		for(j=0;j<10;j++){
-			for(k=0;k<10;k++){	
+	for(i=0;i<vNum;i++){
+		for(j=0;j<vNum;j++){
+			for(k=0;k<vNum;k++){	
 				if(D[j][i] + D[i][k] < D[j][k]){
 					D[j][k] = D[j][i] + D[i][k];
 				}		
@@ -131,11 +133,26 @@ void Floyd(int matrix[][11]){
 		}
 	}
 
-	for(i=0;i<10;i++){
-		for(j=0;j<10;j++){
-			printf("%d ",D[i][j]);
+	printf("\t");
+	for(i=0;i<vNum;i++){
+		printf("%-10s",order[i]);
+	}
+	printf("\n");
+
+
+	for(i=0;i<vNum+1;i++){
+		if(i==0) continue;
+		else{
+			for(j=0;j<vNum+1;j++){
+				if(j==0){
+					printf("%-10s",order[i-1]);
+				}
+				else{
+					printf("%-10d",D[i-1][j-1]);
+				}
+			}
+			printf("\n");
 		}
-		printf("\n");
 	}
 	
 }
@@ -143,20 +160,18 @@ void Floyd(int matrix[][11]){
 int main() {
 
 	FILE *fp ; //파일 포인터
-        fp = fopen("hw5.data","r");
+        fp = fopen("test.data","r");
 
-        char storage[11][11][100];
+        char storage[30][30][100];
 	char * token;
 	char string[100];
-	char index[10][100]; //index for vertex name since we'll consider all vertex as integer in the program.
 	int i;
 	int j;
-	int cnt=0;
 
 	if( fp != NULL )
 	{
 		i=0;
-		while(i<11)
+		while(!feof(fp))
 		{
 			fgets(string,100,fp );
 			
@@ -165,8 +180,8 @@ int main() {
 			while( token != NULL ){
 				strcpy(storage[i][j], token);
 				if(i==0){
-					strcpy(index[j],token);
-					cnt++;
+					strcpy(order[j],token);
+					vNum++;
 				}
 				token = strtok(NULL, " \t\n\v\f\r");
 				j++;
@@ -174,10 +189,9 @@ int main() {
 			}
 			i++;		
 		}
-
 	}
 
-//	for(i=0;i<10;i++) printf("%s\n",index[i]);
+//	for(i=0;i<11;i++) printf("%s\n",index[i]);
 
 /*	for(i=0;i<11;i++){
 		for(j=0;j<11;j++){
@@ -186,9 +200,9 @@ int main() {
 		printf("\n");
 	}*/
 
-	int matrix[11][11];
-	for(i=1;i<11;i++){
-		for(j=1;j<11;j++){
+	int matrix[30][30];
+	for(i=1;i<vNum+1;i++){
+		for(j=1;j<vNum+1;j++){
 			if(strcmp(storage[i][j],"INF")==0)
 				matrix[i][j] = 10000;
 			else 
@@ -196,9 +210,9 @@ int main() {
 		}
 	}
 
-	for(i=1;i<11;i++)
+	for(i=1;i<vNum+1;i++)
 		matrix[0][i] = i-1;
-	for(i=1;i<11;i++)
+	for(i=1;i<vNum+1;i++)
 		matrix[i][0] = i-1;
 
 /*	for(i=0;i<11;i++){
@@ -212,12 +226,30 @@ int main() {
 	clock_t begin = clock();
 	printf("Dijkstra Algorithm Applied |V| times: \n\n");	
 	/* Apply dijkstra |V| times */
-	for(i=0;i<10;i++){
-		dijkstra(matrix,i);
-		for(j=0;j<10;j++){
-			printf("%d ",d[j]);
+
+	printf("\t");	
+	for(i=0;i<vNum;i++){
+		printf("%-10s",order[i]);
+	}
+	printf("\n");
+
+	for(i=0;i<vNum+1;i++){
+
+		if(i==0)
+			continue;
+		else{
+	
+			dijkstra(matrix,i-1);
+			for(j=0;j<vNum+1;j++){
+				if(j==0){
+					printf("%-10s", order[i-1]);
+				}
+				else{
+					printf("%-10d",d[j-1]);
+				}
+			}
+			printf("\n");
 		}
-		printf("\n");
 	}
 
 	clock_t end = clock();
@@ -228,8 +260,8 @@ int main() {
 	/* Calculate Edges and its weight from Adj.Matrix */
 	int size = 0;
 	int k=2;
-	for(i=1;i<11;i++){
-		for(j=k;j<11;j++){
+	for(i=1;i<vNum+1;i++){
+		for(j=k;j<vNum+1;j++){
 			if(matrix[i][j]>0 && matrix[i][j]<10000){
 				edge[size].vertex[0] = i-1;
 				edge[size].vertex[1] = j-1;
@@ -252,15 +284,32 @@ int main() {
 	begin = clock();
 	printf("Bellman-Ford Alogirhtm Applied |V| times: \n\n");
 	/* Apply Bellman-Ford |V| times */
-	for(i=0;i<10;i++){
-		if(Bellman_Ford(i,size)){
-			for(j=0;j<10;j++){
-				printf("%d ",d[j]);	
-			}	
-			printf("\n");
-		}
+
+	printf("\t");	
+	for(i=0;i<vNum;i++){
+		printf("%-10s",order[i]);
+	}
+	printf("\n");
+
+	for(i=0;i<vNum+1;i++){
+
+		if(i==0)
+			continue;
 		else{
-			printf("negative cycle exists when source is %d!\n",i);
+			if(Bellman_Ford(i-1,size)){
+				for(j=0;j<vNum+1;j++){
+					if(j==0){
+						printf("%-10s", order[i-1]);
+					}
+					else{
+						printf("%-10d",d[j-1]);	
+					}
+				}	
+				printf("\n");
+			}
+			else{
+				printf("negative cycle exists when source is %d!\n",i);
+			}
 		}
 	}
 	end = clock();
